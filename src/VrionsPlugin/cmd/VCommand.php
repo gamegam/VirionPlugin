e45da7b22eb12d6b0f64a791e82782f3be43168b
플그러 <48847058+gamegam@users.noreply.github.com>
2023-06-01 21:38:44 +0900

    Create VCommand.php

---
+++ src/VrionsPlugin/cmd/VCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace VrionsPlugin\cmd;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+
+class VCommand extends Command{
+    
+    public function __construct(){   
+        parent::__construct("virions", "View the list of loaded Vrions", null, ["vr"]);     
+        $this->setPermission("vr.cmd");     
+    }
+    public function execute(CommandSender $p, string $label, array $args): bool{
+        $api = Server::getInstance()->getPluginManager()->getPlugin("VrionsPlugin");
+        if (! Server::getInstance()->isOP($p->getName())){
+            return false;
+        }
+        $api->file($p);
+        return false;
+    }
+}
